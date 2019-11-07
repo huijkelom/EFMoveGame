e2f15faef071af5a00d18beffb1c00e726a513cc
Milly <milabits@gmail.com>
2019-11-07 16:52:48 +0100

    Added project files

---
+++ Move Game/Assets/Scripts/TeamButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TeamButton : MonoBehaviour
+{
+	[SerializeField]
+	private Color color = default;
+
+	[SerializeField]
+	private Image image = default;
+}
