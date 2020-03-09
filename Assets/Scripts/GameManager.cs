4be0bc89575033d2602fd5fbe0f77f41bb894abc
Sven Janssen <svenjanssen12321@gmail.com>
2020-03-09 15:42:36 +0100

    Merged designer-hotfix into master

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+	private int playerCount = default;
+
+	private List<TeamCharacter> teams = new List<TeamCharacter>();
+
+	[Header("References")]
+	[SerializeField]
+	private RectTransform replayButton = default;
+	[SerializeField]
+	private GameTimer timer = default;
+	[Space]
+	[SerializeField]
+	private RectTransform teamContainer = default;
+	[SerializeField]
+	private Transform charContainer = default;
+	[SerializeField]
+	private Transform flagContainer = default;
+	[Space]
+	[SerializeField]
+	private TeamCharacter teamPrefab = default;
+	[SerializeField]
+	private AudioSource audioSource = default;
+	[Header("Countdown")]
+	/*[SerializeField]
+	private TextMeshProUGUI countDownText = default;
+	[SerializeField]
+	private RectTransform countDownButton = default;*/
+	/*[SerializeField]
+	private AudioClip countdownBeep = default;
+	[SerializeField]
+	private AudioClip countdownBeepLast = default;*/
+
+	private int _CurrentPlace = 0;
+	private bool _started = false;
+	private static readonly int Blinking = Animator.StringToHash("Blink");
+
+	public GameObject bgMusic;
+
+	private void Start()
+	{
+		playerCount = int.Parse(GlobalGameSettings.GetSetting("Players"));
+
+		for (int i = 0; i < playerCount; i++)
+		{
+			TeamCharacter player = Instantiate(teamPrefab, teamContainer);
+			player.Init(i, charContainer.GetChild(i).GetComponent<Animator>(),
+						flagContainer.GetChild(i).GetComponent<Flag>());
+
+			player.doneEvent.AddListener(TeamFinished);
+
+			teams.Add(player);
+		}
+
+		for (int i = playerCount; i < 4; i++)
+		{
+			Destroy(charContainer.GetChild(i).gameObject);
+		}
+
+		for (int i = 0; i < charContainer.childCount; i++)
+		{
+			charContainer.GetChild(i).GetComponent<CharacterColor>()
+						 .SetColor(PlayerColourContainer.GetPlayerColour(i + 1));
+		}
+
+		timer.TimerRanOut.AddListener(TimeUp);
+	}
+
+	private void TimeUp()
+	{
+		foreach (TeamCharacter team in teams)
+		{
+			team.Deactivate();
+		}
+
+		/*countDownText.text = "Time's Up!";
+		countDownButton.gameObject.SetActive(true);
+		countDownButton.GetComponent<Animator>().SetBool(Blinking, true);*/
+		replayButton.gameObject.SetActive(true);
+	}
+
+	// called through event in scene
+	public void StartGame(int delay)
+	{
+		if (_started) return;
+		_started = true;
+		StartCoroutine(CountDown(delay));
+	}
+
+	public IEnumerator CountDown(int delay)
+	{
+		for (int i = 0; i < delay; i++)
+		{
+			//audioSource.PlayOneShot(countdownBeep);
+			//countDownText.text = (delay - i).ToString();
+			yield return new WaitForSeconds(1);
+		}
+
+		//audioSource.PlayOneShot(countdownBeepLast);
+		//countDownButton.gameObject.SetActive(false);
+		// Make player buttons interactable
+		foreach (TeamCharacter team in teams) team.Activate();
+		bgMusic.SetActive(true);
+		timer.StartTimer();
+	}
+
+	private void TeamFinished(TeamCharacter winner)
+	{
+		_CurrentPlace++;
+		winner.Flag.Init(_CurrentPlace, timer.TimePassed);
+		winner.GetButton().gameObject.SetActive(false);
+		audioSource.Play();
+
+		if (_CurrentPlace >= playerCount)
+		{
+			timer.PauseTimer(true);
+			replayButton.gameObject.SetActive(true);
+		}
+	}
+}
---
+++ Assets/Scripts/TeamButton.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TeamButton : MonoBehaviour, I_SmartwallInteractable
+{
+	[SerializeField]
+	private Image image = default;
+
+	[SerializeField]
+	private TextMeshProUGUI text = default;
+
+	private bool pressed = false;
+
+	private UnityEvent hitEvent = new UnityEvent();
+
+	private EventSystem eventSystem;
+
+	Button _ButtonImOn;
+	Animator _Anime;
+	public AudioSource _AS;
+	public string AnimationTriggerName = "Clicked";
+
+	public void Init(TeamCharacter teamCharacter, Color color)
+	{
+		image.color = color;
+		hitEvent.AddListener(teamCharacter.Hit);
+	}
+
+	public void SetText(string value) => text.text = value;
+
+	private void Awake()
+	{
+		_ButtonImOn = gameObject.GetComponent<Button>();
+		_Anime = gameObject.GetComponent<Animator>();
+		_AS = gameObject.GetComponent<AudioSource>();
+		eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+	}
+
+	public void Hit(Vector3 hitPosition)
+	{
+		if (!pressed)
+		{
+			hitEvent.Invoke();
+			StartCoroutine(blockPresses(.25f));
+			if (_AS != null) _AS.Play();
+			if (_Anime != null) _Anime.SetTrigger(AnimationTriggerName);
+			//_ButtonImOn.onClick.Invoke()
+			ExecuteEvents.Execute(_ButtonImOn.gameObject, new BaseEventData(eventSystem), ExecuteEvents.submitHandler);
+		}
+	}
+
+	private IEnumerator blockPresses(float delay)
+	{
+		pressed = true;
+		yield return new WaitForSeconds(delay);
+		pressed = false;
+	}
+}
